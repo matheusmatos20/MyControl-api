5873087206aed43e3503c0427161abc57d418e21
matheusmatos20 <matheus.matos@fatec.sp.gov.br>
2025-07-02 23:25:20 -0300

    ServicoCliente

--- app/Model/Servico.py
+++ app/Model/Servico.py
@@ -2,7 +2,7 @@ import pyodbc
 import pandas as pd
 from datetime import datetime
 from Model import Conn_DB
-from Schemas import Servico as servico
+from Schemas import Servico as servico,ServicoCliente as servico_cliente
 
 class ServicoDAL:
     def __init__(self):
@@ -68,3 +68,60 @@ class ServicoDAL:
                 return True
         except Exception:
             return False
+
+    def inserir_servico_cliente(self,servico_cliente):
+        query = f""" 
+
+        INSERT INTO TB_SERVICOS_CLIENTE(ID_CLIENTE" +
+                    "                                                   ,ID_SERVICO" +
+                    "                                                   ,VL_SERVICO" +
+                    "                                                   ,VL_DESCONTO" +
+                    "                                                   ,ID_USUARIO) " +
+                    "SELECT " + {servico_cliente.id_cliente} +
+                    "," {servico_cliente.id_servico}
+                    ",convert(numeric(19,2),REPLACE('" + {servico_cliente.vl_servico} + "',',','.'))"+
+                    ",convert(numeric(19,2),REPLACE('" + {servico_cliente.vl_desconto} + "',',','.'))" +
+                    ","+{servico_cliente.id_usuario} """
+        print(query)
+        try:
+            with self._connect() as conn:
+                cursor = conn.cursor()
+                cursor.execute(query)
+                conn.commit()
+                return True
+        except Exception:
+            return False
+        
+    def retorna_servicos_cliente(self):
+        query = """
+        SELECT C.ID_CLIENTE                   AS ID
+              ,C.NM_CLIENTE                 AS Cliente
+              ,S.DS_SERVICO                 AS Serviço
+              ,SC.DT_SERVICO                AS 'Data Inicio'
+              ,SC.VL_SERVICO                AS Valor 
+              ,SC.VL_DESCONTO               AS Desconto
+              ,U.NM_USUARIO                 AS 'Usuário'
+        FROM TB_CLIENTES         C WITH(NOLOCK)
+        JOIN TB_SERVICOS_CLIENTE SC WITH(NOLOCK) ON SC.ID_CLIENTE = C.ID_CLIENTE
+        JOIN TB_SERVICOS		   S WITH(NOLOCK) ON S.ID_SERVICO = SC.ID_SERVICO
+        JOIN TB_USUARIOS         U WITH(NOLOCK) ON C.ID_USUARIO = SC.ID_USUARIO
+        WHERE S.FL_RECORRENTE = 0  AND CONVERT(DATE,DT_SERVICO,103)=CONVERT(DATE,GETDATE(),103)
+                                                  
+        UNION
+                                                  
+        SELECT C.ID_CLIENTE       AS ID
+              ,C.NM_CLIENTE             AS Cliente
+              ,S.DS_SERVICO             AS Serviço
+              ,SC.DT_SERVICO            AS 'Data Inicio'
+              ,SC.VL_SERVICO            AS Valor 
+              ,SC.VL_DESCONTO           AS Desconto
+              ,U.NM_USUARIO                 AS 'Usuário'
+        FROM TB_CLIENTES C WITH(NOLOCK)
+        JOIN TB_SERVICOS_CLIENTE SC WITH(NOLOCK) ON SC.ID_CLIENTE = C.ID_CLIENTE
+        JOIN TB_SERVICOS		 S WITH(NOLOCK) ON S.ID_SERVICO = SC.ID_SERVICO
+        JOIN TB_USUARIOS         U WITH(NOLOCK) ON C.ID_USUARIO = SC.ID_USUARIO
+        WHERE S.FL_RECORRENTE = 1   
+
+        """
+        with self._connect() as conn:
+            return pd.read_sql(query, conn)
